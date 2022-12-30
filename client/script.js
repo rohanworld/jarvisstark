cd64ff52f40f54b2bfd083daf1a8c3b9771c42fd
ROhan <rohanmaharaj78@gmail.com>
2022-12-30 14:38:06 +0530

    Next Commit after adding render backend connection

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //frtch the data from the server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://jarvisai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
